758a4280783c5183b314baadf5a08ba95babd08d
Seiji Tonouchi <seiji@pop-os.localdomain>
2020-12-18 19:05:11 +1300

    better linking

--- mklink.sh
+++ mklink.sh
@@ -2,29 +2,28 @@
 
 SCRIPT_ABS_PATH=`realpath .`
 
-for i in `find ${SCRIPT_ABS_PATH}/ -type d -name .git -prune -o -type d -print`
-do 
-  PATH_NAME=`basename ${i}`
-  [ ${PATH_NAME} = "dotfiles" ] && continue
+for i in `find . -type d | grep -v "\.git"`
+do
+  [[ ${i} =~ ^\.$ ]] && continue
 
-  if [ ! -d ${HOME}/${PATH_NAME} ]
-  then
-    mkdir ${HOME}/${PATH_NAME}
+  if [ -d ${HOME}/${i} ]
+  then 
+    echo "${HOME}/${i} exists"
+  else
+    echo "${HOME}/${i} not exist"
+    mkdir -p ${HOME}/${i}
   fi
-  for i in `ls ${PATH_NAME}`
-  do
-    ln -snfv ${SCRIPT_ABS_PATH}/${PATH_NAME}/${i} ${HOME}/${PATH_NAME}/${i} > /dev/null 2>&1
-  done
 done
 
-for i in `find ${SCRIPT_ABS_PATH} -maxdepth 1 -type d -name .git -prune -o -type f -print`
-do
-  FILE_NAME=`basename ${i}`
-  [[ ${FILE_NAME} =~ ^.+\/dotfiles$ ]] && continue
-  [[ ${FILE_NAME} =~ ^.gitignore$ ]] && continue
-  [[ ${FILE_NAME} =~ ^mklink.sh$ ]] && continue
-  [[ ${FILE_NAME} =~ ^README.md$ ]] && continue
-  [[ ${FILE_NAME} =~ ^.+\.swp$ ]] && continue
 
-  ln -snfv ${SCRIPT_ABS_PATH}/${FILE_NAME} ${HOME}/${FILE_NAME} > /dev/null 2>&1
+for i in `find . -type f | grep -v "\.git"`
+do
+  [[ ${i} =~ ^.+mklink\.sh$ ]] && continue
+  [[ ${i} =~ ^.+README\.md$ ]] && continue
+  [[ ${i} =~ ^.+lost\+found ]] && continue
+  if [ -f ${HOME}/${i} ]
+  then
+    rm ${HOME}/${i}
+  fi
+  ln -snfv $(realpath ${i}) ${HOME}/${i}
 done
